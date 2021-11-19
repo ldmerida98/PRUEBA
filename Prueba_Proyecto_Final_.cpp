3bf85088c1b4bafc1ddf91319b4e1efe0cd7bb4b
Eduardo-Arks <93789290+Eduardo-Arks@users.noreply.github.com>
2021-11-18 23:00:13 -0600

    sus

--- Prueba_Proyecto_Final_.cpp
+++ Prueba_Proyecto_Final_.cpp
@@ -2,11 +2,10 @@
 #include <time.h>
 #include <stdlib.h>
 
-int congruencia(int a,int k, int d);
 int main() {//Daniel
 	system("color 40");
-	int anio,;
-	int opcion=1, dia=1, mes=1;
+	int anio, febrero;
+	int opcion=1, dias_mes, dia=1, mes=1;
   
   time_t fecha; //*Inicia Daniel =)
 	struct tm *ahora;
@@ -14,9 +13,4 @@ int main() {//Daniel
 	ahora=localtime(&fecha); 
 	dia=ahora->tm_mday;
 	mes=ahora->tm_mon+1; 
-	anio = (ahora->tm_year)+1900;
-	system ("cls"); 
-	printf("\t\t\t--------------------CALENDARIO--------------------\n\n");
-	printf(" > La fecha de hoy es: %d/%d/%d \n\n\n",dia,mes,anio); 
-	while (opcion != 0) 
-	{
+	anio = (ahora->tm_year)+1900; 
